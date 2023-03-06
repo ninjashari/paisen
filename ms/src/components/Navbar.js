123860939d6cfb569653bd1adf0e29ae6ec4192f
Abhinav Aggarwal <abhinavaggarwal23@yahoo.com>
2023-03-06 22:19:58 +0530

    added bootstrap 5 and Navbar

---
+++ ms/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+          <a class="navbar-brand" href="/">
+            SAMLS
+          </a>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
--- ms/src/index.js
+++ ms/src/index.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Navbar from "./components/Navbar";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-   Hello World!
+    <Navbar />
   </React.StrictMode>
 );
