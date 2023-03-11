8f176a9224778ffbdb44a2a55255ab877e119b11
Abhinav Aggarwal <abhinavaggarwal23@yahoo.com>
2023-03-11 18:18:37 +0530

    removed unneeded import

--- ms/src/SecuredApp.js
+++ ms/src/SecuredApp.js
@@ -2,8 +2,6 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Login from "./components/Login"
 
-import data from './utils/secret.json'
-
 function SecuredApp() {
   return (
     <div>
