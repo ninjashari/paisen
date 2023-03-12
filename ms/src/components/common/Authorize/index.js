2bff61f0d4a828fb6c58cdaf2f2a5cd2b0fcc3b6
Abhinav Aggarwal <abhinavaggarwal23@yahoo.com>
2023-03-12 23:24:58 +0530

    added authorization

--- ms/src/components/common/Authorize/index.js
+++ ms/src/components/common/Authorize/index.js
@@ -1,21 +1,46 @@
-import React from "react"
+import React, { useState } from "react"
 
 function Authorize() {
+  const [username, setUsername] = useState("")
+
+  const clientId = process.env.REACT_APP_MYANIMELIST_CLIENT_ID
+  console.log(clientId)
+
+  const pkceCodeChallenge = "47DEQpj8HBSa-_TImW-5JCeuQeRkm5NMpJWZG3hSuFU"
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    const url =
+      "https://myanimelist.net/v1/oauth2/authorize?response_type=code&client_id=" +
+      clientId +
+      "&code_challenge=" +
+      pkceCodeChallenge +
+      "&code_challenge_method=plain"
+
+    window.open(url)
+  }
+
   return (
     <div className="container" style={{ marginTop: "10rem" }}>
       <div className="row align-items-start">
         <div className="col"></div>
         <div className="col-4">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="form-floating mb-3">
               <input
                 type="text"
                 className="form-control"
-                id="floatingInput"
                 placeholder="Username"
                 aria-describedby="usernameHelp"
+                value={username}
+                onChange={handleUsernameChange}
               />
-              <label for="floatingInput">Username</label>
+              <label>Username</label>
               <div
                 id="usernameHelp"
                 className="form-text text-color-white"
@@ -28,7 +53,7 @@ function Authorize() {
               className="w-100 btn btn-lg btn-secondary mb-3"
               type="submit"
             >
-              Submit
+              Authorize
             </button>
           </form>
         </div>
---
+++ ms/src/pages/Authorized/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Header from "../../components/layout/Header"
+
+function Authorized() {
+  return (
+    <Header />
+    
+  )
+}
+
+export default Authorized
