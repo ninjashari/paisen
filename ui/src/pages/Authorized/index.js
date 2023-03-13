bb9ded4993e5bad425f9d896112c14ca7d4be287
Abhinav Aggarwal <abhinavaggarwal23@yahoo.com>
2023-03-13 16:36:45 +0530

    changed folder name for UI code

--- ui/src/pages/Authorized/index.js
+++ ui/src/pages/Authorized/index.js
@@ -1,12 +1,17 @@
-import React from "react"
+import React, { useState } from "react"
 import { useSearchParams } from "react-router-dom"
 import Header from "../../components/layout/Header"
 
 function Authorized() {
   const [searchParams, setSearchParams] = useSearchParams("")
+  const [code, setCode] = useState()
+  const [username, setUsername] = useState()
 
   console.log(searchParams.get("code"))
   console.log(searchParams.get("state"))
+
+  setCode(searchParams.get("code"))
+  setUsername(searchParams.get("state"))
 
   return <Header />
 }
