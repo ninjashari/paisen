6fc200c8c64f8ca320afc62c94d33d89beb0607c
Abhinav Aggarwal <abhinavaggarwal23@yahoo.com>
2023-03-16 11:01:09 +0530

    updating user and redirect to animelist page

--- ui/src/pages/Authorized/index.js
+++ ui/src/pages/Authorized/index.js
@@ -1,13 +1,21 @@
 import { useSearchParams } from "react-router-dom"
+import Header from "../../components/layout/Header"
+import { updateUser } from "../../utils/api"
 
-function Authorized() {
+async function Authorized() {
   const [searchParams] = useSearchParams("")
 
-  console.log("code :: ", searchParams.get("code"))
+  const code = searchParams.get("code")
+  const state = searchParams.get("state")
+
+  const response = await updateUser(state, code)
+
+  console.log("response :: ", response)
   console.log("username :: ", searchParams.get("state"))
 
-  const code = searchParams.get("code")
-  const username = searchParams.get("state")
+  window.location.href = "http://localhost:3000/animelist"
+
+  return <Header />
 }
 
 export default Authorized
