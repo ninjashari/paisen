e8a31a563296a7e0dfdc0a90ab782bad4747163c
Abhinav Aggarwal <abhinavaggarwal23@yahoo.com>
2023-03-16 10:34:31 +0530

    reverted to previous file extension

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,8 +1,8 @@
 import express from "express"
 import cors from "cors"
-import "./loadEnvironment.js"
+import "./loadEnvironment.mjs"
 import "express-async-errors"
-import users from "./routes/users.js"
+import users from "./routes/users.mjs"
 
 const PORT = process.env.PORT || 5050
 const app = express()
