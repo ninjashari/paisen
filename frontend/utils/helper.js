26b8b7f7246f17a600f53e202827c1bf6c0ebc70
Abhinav Aggarwal <abhinavaggarwal23@yahoo.com>
2023-04-08 21:37:52 +0530

    changes for mal code fetch from uri

--- frontend/utils/helper.js
+++ frontend/utils/helper.js
@@ -3,3 +3,13 @@ export const camelize = (str) => {
     return str.toUpperCase()
   })
 }
+
+export const getQueryParams = (url) => {
+  const paramArr = url.slice(url.indexOf("?") + 1).split("&")
+  const params = {}
+  paramArr.map((param) => {
+    const [key, val] = param.split("=")
+    params[key] = decodeURIComponent(val)
+  })
+  return params
+}
