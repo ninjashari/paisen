6153146850845b54fb580af3d4413e3970747153
Abhinav Aggarwal <abhinavaggarwal23@yahoo.com>
2023-04-08 19:01:40 +0530

    sidebar changes for UX navigation

--- frontend/components/sidebar.jsx
+++ frontend/components/sidebar.jsx
@@ -1,10 +1,61 @@
 import Link from "next/link"
-import { useState } from "react"
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
 
 const Sidebar = () => {
+  const router = useRouter()
+
+  const [homeClass, setHomeClass] = useState("nav-link collapsed")
+  const [statisticsClass, setStatisticsClass] = useState("nav-link collapsed")
+  const [searchClass, setSearchClass] = useState("nav-link collapsed")
+  const [seasonsClass, setSeasonsClass] = useState("nav-link collapsed")
+  const [jellyfinClass, setJellyfinClass] = useState("nav-link collapsed")
+
   const [animeListClass, setAnimeListClass] = useState("nav-content collapse")
   const [animeListLinkClass, setAnimeListLinkClass] =
     useState("nav-link collapsed")
+
+  const [currentAnimeListClass, setCurrentAnimeListClass] = useState("")
+  const [completedAnimeListClass, setCompletedAnimeListClass] = useState("")
+  const [onholdAnimeListClass, setOnholdAnimeListClass] = useState("")
+  const [droppedAnimeListClass, setDroppedAnimeListClass] = useState("")
+  const [plantowatchAnimeListClass, setPlantowatchAnimeListClass] = useState("")
+
+  useEffect(() => {
+    const currentPath = router?.asPath
+
+    if ("/" === currentPath) {
+      setHomeClass("nav-link")
+    } else if ("/statistics" === currentPath) {
+      setStatisticsClass("nav-link")
+    } else if ("/search" === currentPath) {
+      setSearchClass("nav-link")
+    } else if ("/seasons" === currentPath) {
+      setSeasonsClass("nav-link")
+    } else if ("/jellyfin" === currentPath) {
+      setJellyfinClass("nav-link")
+    } else if ("/animelist/current" === currentPath) {
+      setAnimeListClass("nav-content")
+      setAnimeListLinkClass("nav-link")
+      setCurrentAnimeListClass("active")
+    } else if ("/animelist/completed" === currentPath) {
+      setAnimeListClass("nav-content")
+      setAnimeListLinkClass("nav-link")
+      setCompletedAnimeListClass("active")
+    } else if ("/animelist/onhold" === currentPath) {
+      setAnimeListClass("nav-content")
+      setAnimeListLinkClass("nav-link")
+      setOnholdAnimeListClass("active")
+    } else if ("/animelist/dropped" === currentPath) {
+      setAnimeListClass("nav-content")
+      setAnimeListLinkClass("nav-link")
+      setDroppedAnimeListClass("active")
+    } else if ("/animelist/plantowatch" === currentPath) {
+      setAnimeListClass("nav-content")
+      setAnimeListLinkClass("nav-link")
+      setPlantowatchAnimeListClass("active")
+    }
+  }, [])
 
   const handleShowAnimeList = (e) => {
     e.preventDefault()
@@ -25,7 +76,7 @@ const Sidebar = () => {
     <aside id="sidebar" className="sidebar">
       <ul className="sidebar-nav" id="sidebar-nav">
         <li className="nav-item">
-          <Link className="nav-link collapsed" href="/">
+          <Link className={homeClass} href="/">
             <i className="bi bi-grid"></i>
             <span>Home</span>
           </Link>
@@ -39,31 +90,37 @@ const Sidebar = () => {
 
           <ul id="components-nav" className={animeListClass}>
             <li>
-              <Link href="/animelist/current" className="active">
+              <Link href="/animelist/current" className={currentAnimeListClass}>
                 <i className="bi bi-circle"></i>
                 <span>Currently Watching</span>
               </Link>
             </li>
             <li>
-              <Link href="/animelist/completed">
+              <Link
+                href="/animelist/completed"
+                className={completedAnimeListClass}
+              >
                 <i className="bi bi-circle"></i>
                 <span>Completed</span>
               </Link>
             </li>
             <li>
-              <Link href="/animelist/onhold">
+              <Link href="/animelist/onhold" className={onholdAnimeListClass}>
                 <i className="bi bi-circle"></i>
                 <span>On Hold</span>
               </Link>
             </li>
             <li>
-              <Link href="/animelist/dropped">
+              <Link href="/animelist/dropped" className={droppedAnimeListClass}>
                 <i className="bi bi-circle"></i>
                 <span>Dropped</span>
               </Link>
             </li>
             <li>
-              <Link href="/animelist/plantowatch">
+              <Link
+                href="/animelist/plantowatch"
+                className={plantowatchAnimeListClass}
+              >
                 <i className="bi bi-circle"></i>
                 <span>Plan To Watch</span>
               </Link>
@@ -72,28 +129,28 @@ const Sidebar = () => {
         </li>
 
         <li className="nav-item">
-          <Link className="nav-link collapsed" href="/statistics">
+          <Link className={statisticsClass} href="/statistics">
             <i className="bi bi-bar-chart"></i>
             <span>Statictics</span>
           </Link>
         </li>
 
         <li className="nav-item">
-          <Link className="nav-link collapsed" href="/search">
+          <Link className={searchClass} href="/search">
             <i className="bi bi-search"></i>
             <span>Search</span>
           </Link>
         </li>
 
         <li className="nav-item">
-          <Link className="nav-link collapsed" href="/seasons">
+          <Link className={seasonsClass} href="/seasons">
             <i className="bi bi-calendar3"></i>
             <span>Seasons</span>
           </Link>
         </li>
 
         <li className="nav-item">
-          <Link className="nav-link collapsed" href="/jellyfin">
+          <Link className={jellyfinClass} href="/jellyfin">
             <i className="bi bi-link"></i>
             <span>Jellyfin</span>
           </Link>
