20c81c821b404fa2d5088cf87834b57b51c23a3c
Abhinav Aggarwal <abhinavaggarwal23@yahoo.com>
2025-06-27 05:26:10 +0000

    Remove deprecated files and components to streamline the project

--- utils/uriService.js
+++
@@ -1,8 +0,0 @@
-export const getURILastValue = (uri) => {
-  if (uri) {
-    const valueArr = uri.split("/")
-    const lastVal = valueArr[valueArr.length - 1]
-    return lastVal
-  }
-  return undefined
-}
